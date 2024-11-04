9bc3632f18debc447204f4d96ba34064cc3ad192
Enoque Mendes <99991157+oenoquemendes@users.noreply.github.com>
2024-11-04 18:42:07 -0300

    Update conexao.php

--- mods/conexao.php
+++ mods/conexao.php
@@ -1,7 +1,7 @@
 <?php
 define( 'MYSQL_HOST', 'localhost' );
 define( 'MYSQL_USER', 'root' );
-define( 'MYSQL_PASSWORD', 'sysbot1' );
+define( 'MYSQL_PASSWORD', '' );
 define( 'MYSQL_DB_NAME', 'inventario' );
 
 try
